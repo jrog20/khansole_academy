805bb9e23f48eb13ee792ddd0eff6f6630534ab3
Jaime Rogstad <66330786+jrog20@users.noreply.github.com>
2021-04-29 22:24:17 -0400

    Update khansole_academy.py

--- khansole_academy.py
+++ khansole_academy.py
@@ -8,35 +8,32 @@ the user answers three correctly in a row.
 """
 
 import random
+
 NUM_MIN = 10
 NUM_MAX = 99
 CORRECT_IN_A_ROW = 3
 
 def main():
-    # start with no correct answers = 0.
     num_correct = 0
 
     while num_correct != CORRECT_IN_A_ROW:
-        # randomly generate two digit addition problems for the user to solve
         num1 = random.randint(NUM_MIN, NUM_MAX)
         num2 = random.randint(NUM_MIN, NUM_MAX)
         total = num1 + num2
 
-        # ask user to solve problem, while the user has not yet answered 3 correctly in a row
-        print("What is " + str(num1) + " + " + str(num2) + "?")
+        print("What is", num1, "+", str(num2) + "?")
         answer = int(input("Your answer: "))
 
-        # check to see if the problem was answered right or wrong by the user
         if answer == total:
-            print("Correct! You've gotten " + str(num_correct + 1) + " correct in a row.")
+            print("Correct! You've gotten", (num_correct + 1), "correct in a row.")
             num_correct += 1
         else:
-            print("Incorrect. The expected answer is " + str(total) + ".")
+            print("Incorrect. The expected answer is", str(total) + ".")
             num_correct = 0
 
-    # message user receives when three answered correctly in a row:
     print("Congratulations! You mastered addition.")
 
 
 if __name__ == '__main__':
     main()
+
